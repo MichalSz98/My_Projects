98309af5398b2265df8cebb47b1b7c2a9feee35e
MichalSz98 <48216093+MichalSz98@users.noreply.github.com>
2020-03-24 09:54:56 +0100

    Added Web Project - "Todo list"

---
+++ Java/Spring/todo_list_project/src/main/java/todo_project/Web_application.java
@@ -0,0 +1,13 @@
+package todo_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web_application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web_application.class, args);
+	}
+
+}
---
+++ Java/Spring/todo_list_project/src/main/resources/db/migration/V5__Add_todos.sql
@@ -0,0 +1,2 @@
+insert into todos (text, done) values ('mock 3', 1);
+insert into todos (text, done) values ('mock 4', 0);
