069c0e8a9b0578eac40a1d62c13e7d992bd2594b
MichalSz98 <48216093+MichalSz98@users.noreply.github.com>
2020-03-24 10:01:41 +0100

    Delete LangRepository.java

--- Java/Spring/hello/src/main/java/io/github/mat3e/lang/LangRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.mat3e.lang;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LangRepository extends JpaRepository<Lang, Integer> {
-
-}
